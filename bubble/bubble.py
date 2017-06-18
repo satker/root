0df948bc8c431b500ced67227974b99ac6f90437
satker <satker73@gmail.com>
2017-06-18 10:56:39 +0300

    upd

--- bubble/bubble.py
+++ bubble/bubble.py
@@ -1,11 +1,14 @@
 class Sort():
     def __init__(self, m):
+        count = 1
         while self.control(m) != 0:
             i = 0
-            while i < (len(m) - 1):
+            while i < (len(m) - count):
                 if m[i] > m[i+1]:
                     m[i], m[i+1] = m[i+1], m[i]
-                i +=  1
+                i += 1
+            count += 1
+
         self.m = m
 
     def control(self, m):
