0ead9bf81f69ca58dd7bcfefbfe2569a2ee88ff1
satker <satker73@gmail.com>
2017-06-18 10:45:45 +0300

    updB

--- bubble/bubble.py
+++ bubble/bubble.py
@@ -1,17 +1,19 @@
-def algoritm(m):
-    while control(m) != 0:
+class Sort():
+    def __init__(self, m):
+        while self.control(m) != 0:
+            i = 0
+            while i < (len(m) - 1):
+                if m[i] > m[i+1]:
+                    m[i], m[i+1] = m[i+1], m[i]
+                i = i + 1
+        print(m)
+
+    def control(self, m):
+        count = 0
         i = 0
         while i < (len(m) - 1):
             if m[i] > m[i+1]:
-                m[i], m[i+1] = m[i+1], m[i]
-            i = i + 1
-    print(m)
+                count = count+1
+            i = i+1
+        return count
 
-def control(k):
-    count = 0
-    i = 0
-    while i < (len(k) - 1):
-        if k[i] > k[i+1]:
-            count = count+1
-        i = i+1
-    return count
